ad5a8554991eac5e4b9f82599e9949ecd97dfb19
Dwyrm <jimmi.schaeffer@ymail.com>
2015-04-15 11:14:43 +0200

    absenceHandler

--- src/handler/AbsenceHandler.java
+++ src/handler/AbsenceHandler.java
@@ -3,7 +3,6 @@ package handler;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import model.Absence;
 import model.Staff;
