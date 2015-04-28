84446de156f5615fbb04d466a3b322a4fe491b3c
Dwyrm <jimmi.schaeffer@ymail.com>
2015-04-28 11:49:14 +0200

    GUI personale knapper virker Handler constructor lavet public

--- src/view/create/CreateStaff.java
+++ src/view/create/CreateStaff.java
@@ -5,7 +5,7 @@
  */
 package view.create;
 
-
+import handler.StaffHandler;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Container;
@@ -25,26 +25,26 @@ import javax.swing.JTextArea;
  * @author Andreas
  */
 public class CreateStaff extends JPanel {
-    
-    public CreateStaff(String opret_medarbejder,JTabbedPane jtp) {
+
+    public CreateStaff(String opret_medarbejder, JTabbedPane jtp) {
         this.setLayout(new BorderLayout());
 
         JPanel CreatePersonaleCenter = new JPanel();
         this.add(CreatePersonaleCenter, BorderLayout.CENTER);
-        
+
         JPanel CreatePersonaleWest = new JPanel();
         this.add(CreatePersonaleWest, BorderLayout.WEST);
 
         JPanel CreatePersonaleSouth = new JPanel();
         this.add(CreatePersonaleSouth, BorderLayout.SOUTH);
-        
+
         JPanel CreatePersonaleEast = new JPanel();
         this.add(CreatePersonaleEast, BorderLayout.EAST);
-        
+
         Container CreatePersonalePaneCenter = new Container();
         CreatePersonalePaneCenter.setLayout(new GridLayout(0, 1, 20, 20));
         CreatePersonaleCenter.add(CreatePersonalePaneCenter);
-        
+
         Container CreatePersonalePaneWest = new Container();
         CreatePersonalePaneWest.setLayout(new GridLayout(0, 1, 20, 20));
         CreatePersonaleWest.add(CreatePersonalePaneWest);
@@ -56,77 +56,89 @@ public class CreateStaff extends JPanel {
         Container CreatePersonalePaneEast = new Container();
         CreatePersonalePaneEast.setLayout(new GridLayout(0, 1, 20, 20));
         CreatePersonaleEast.add(CreatePersonalePaneEast);
-        
+
         JLabel fornavn = new JLabel("Fornavn");
         CreatePersonalePaneWest.add(fornavn);
-        
+
         JLabel efternavn = new JLabel("Efternavn");
         CreatePersonalePaneWest.add(efternavn);
-        
+
         JLabel kaldenavn = new JLabel("Kaldenavn");
         CreatePersonalePaneWest.add(kaldenavn);
-        
+
         JLabel telefon = new JLabel("Tlf");
         CreatePersonalePaneWest.add(telefon);
-        
+
         JLabel mobil = new JLabel("Mobil");
         CreatePersonalePaneWest.add(mobil);
-        
+
         JLabel email = new JLabel("Email");
         CreatePersonalePaneWest.add(email);
-        
+
         JLabel stilling = new JLabel("Stilling");
         CreatePersonalePaneWest.add(stilling);
-        
+
         JLabel modul = new JLabel("Modul");
         CreatePersonalePaneWest.add(modul);
-        
+
         JCheckBox student = new JCheckBox("Student");
         CreatePersonalePaneWest.add(student);
-        
+
         JTextArea fornavninfo = new JTextArea("     ");
         CreatePersonalePaneCenter.add(fornavninfo);
-        
+
         JTextArea efternavninfo = new JTextArea("    ");
         CreatePersonalePaneCenter.add(efternavninfo);
-        
+
         JTextArea kaldenavninfo = new JTextArea("    ");
         CreatePersonalePaneCenter.add(kaldenavninfo);
-        
+
         JTextArea telefonnr = new JTextArea("    ");
         CreatePersonalePaneCenter.add(telefonnr);
-        
+
         JTextArea mobilnr = new JTextArea("    ");
         CreatePersonalePaneCenter.add(mobilnr);
-        
+
         JTextArea emailinfo = new JTextArea("    ");
         CreatePersonalePaneCenter.add(emailinfo);
-        
+
         JComboBox stillingListe = new JComboBox();
         CreatePersonalePaneCenter.add(stillingListe);
-        
+
+        stillingListe.addItem("Afdelingssygeplejeske");
+        stillingListe.addItem("Afdelingsadminstrativ");
+        stillingListe.addItem("Sygeplejeske");
+        stillingListe.addItem("Radiograf");
+        stillingListe.addItem("Sygehjælper");
+        stillingListe.addItem("SOSU-assistent");
+
         JComboBox modulListe = new JComboBox();
         CreatePersonalePaneCenter.add(modulListe);
-                
-        JButton tilbage6 = new JButton("Tilbage til Personaleoversigt");
-        tilbage6.addActionListener(new ActionListener(){
-        
-        
+
+        for (int i = 3; i < 11; i++) {
+            modulListe.addItem(i);
+        }
+
+        JButton tilbage = new JButton("Annuller");
+        tilbage.addActionListener(new ActionListener() {
+
             @Override
             public void actionPerformed(ActionEvent e) {
                 jtp.setSelectedIndex(3);
             }
         });
-        CreatePersonaleSouth.add(tilbage6);
-        
-        JButton gem6 = new JButton("Gem medarbejder");
-        gem6.addActionListener(new ActionListener(){
-            
+        CreatePersonaleSouth.add(tilbage);
+
+        JButton gem = new JButton("Gem medarbejder");
+        gem.addActionListener(new ActionListener() {
+
             @Override
             public void actionPerformed(ActionEvent e) {
                 jtp.setSelectedIndex(3);
+                StaffHandler sh = new StaffHandler();
+//                sh.insertStaff(1, fornavn.getText(), efternavn.getText(), kaldenavn.getText());
             }
         });
-        CreatePersonaleSouth.add(gem6);
+        CreatePersonaleSouth.add(gem);
     }
 }
--- src/view/show/ShowCompetence.java
+++ src/view/show/ShowCompetence.java
@@ -17,7 +17,6 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import static sun.net.www.http.HttpClient.New;
 
 /**
  *
