bf4588e110bd77979bd9c48dc4d25547ce17a7a0
Birgit Schæffer <Birgit Schffer@10.200.124.37>
2015-05-07 13:51:36 +0200

    statestik oprettet

--- src/view/show/ShowStatestik.java
+++ src/view/show/ShowStatestik.java
@@ -5,17 +5,22 @@
  */
 package view.show;
 
+import handler.RoomHandler;
+import handler.StaffHandler;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import model.Room;
+import model.Staff;
 
 /**
  *
@@ -55,6 +60,11 @@ public class ShowStatestik extends JPanel {
 
             }
         });
+        
+        typecombobox.addItem("Dagsvagt");
+        typecombobox.addItem("Aftenvagt");
+        typecombobox.addItem("Nattevagt");
+        typecombobox.addItem("Fravær");
 
         JLabel statmedarbejder = new JLabel("Medarbejder");
         statestikNorth.add(statmedarbejder);
@@ -68,6 +78,16 @@ public class ShowStatestik extends JPanel {
 
             }
         });
+        
+        StaffHandler sh = new StaffHandler();
+        ArrayList<Staff> medarbejdere = new ArrayList<>();
+        medarbejdere = sh.getStaff();
+
+        for (int i = 0; i < medarbejdere.size(); i++) {
+
+            arbejdercombobox.addItem(medarbejdere.get(i).getFirstName() + " "
+                    + medarbejdere.get(i).getLastName());
+        }
 
         JLabel statrum = new JLabel("Rum");
         statestikNorth.add(statrum);
@@ -81,30 +101,265 @@ public class ShowStatestik extends JPanel {
 
             }
         });
+        
+        RoomHandler rh = new RoomHandler();
+        ArrayList<Room> rum = new ArrayList<>();
+        rum = rh.getRoom();
 
+        for (int i = 0; i < rum.size(); i++) {
+
+            rumcombobox.addItem(rum.get(i).getId());
+        }
+                        
         JLabel statstart = new JLabel("Startdato");
         statestikNorth.add(statstart);
-
-        JComboBox startdatocombobox = new JComboBox();
-        statestikNorth.add(startdatocombobox);
-        startdatocombobox.addActionListener(new ActionListener() {
+        
+        JPanel statestikx = new JPanel();
+        statestikNorth.add(statestikx);
+        
+        JLabel startday = new JLabel("Dag: ");
+        statestikx.add(startday);
+        
+        JComboBox startdaycombobox = new JComboBox();
+        statestikx.add(startdaycombobox);
+        startdaycombobox.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
-
-        JLabel statslut = new JLabel("Slutdato");
-        statestikNorth.add(statslut);
-
-        JComboBox slutdatocombobox = new JComboBox();
-        statestikNorth.add(slutdatocombobox);
-        slutdatocombobox.addActionListener(new ActionListener() {
+        
+        for (int i = 1; i <= 31; i++) {
+            startdaycombobox.addItem(i);
+        }
+        
+        JLabel startmonth = new JLabel("Måned: ");
+        statestikx.add(startmonth);
+        
+        JComboBox startmonthcombobox = new JComboBox();
+        statestikx.add(startmonthcombobox);
+        startmonthcombobox.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+        
+        for (int i = 1; i <= 12; i++) {
+            startmonthcombobox.addItem(i);
+        }
+        
+        JLabel startyear = new JLabel("År: ");
+        statestikx.add(startyear);
+        
+        JComboBox startyearcombobox = new JComboBox();
+        statestikx.add(startyearcombobox);
+        startyearcombobox.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        
+        for (int i = 2015; i <= 2021; i++) {
+            startyearcombobox.addItem(i);
+        }
+        
+        startmonthcombobox.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int year = Integer.parseInt(String.valueOf(startyearcombobox.getSelectedItem()));
+                int month = Integer.parseInt(String.valueOf(startmonthcombobox.getSelectedItem()));
+                int daysInMonth = 0;
+                switch (month) {
+                    case 1:
+                        daysInMonth = 31;
+                        break;
+                    case 2:
+                        daysInMonth = (year % 4 == 0) && (year % 100 != 0)
+                                || (year % 400 == 0) ? 29 : 28;
+                        break;
+                    case 3:
+                        daysInMonth = 31;
+                        break;
+                    case 4:
+                        daysInMonth = 30;
+                        break;
+                    case 5:
+                        daysInMonth = 31;
+                        break;
+                    case 6:
+                        daysInMonth = 30;
+                        break;
+                    case 7:
+                        daysInMonth = 31;
+                        break;
+                    case 8:
+                        daysInMonth = 31;
+                        break;
+                    case 9:
+                        daysInMonth = 30;
+                        break;
+                    case 10:
+                        daysInMonth = 31;
+                        break;
+                    case 11:
+                        daysInMonth = 30;
+                        break;
+                    case 12:
+                        daysInMonth = 31;
+                        break;
+                    default:
+                        daysInMonth = 30;
+                }
+                startdaycombobox.removeAllItems();
+                for (int i = 1; i <= daysInMonth; i++) {
+                    startdaycombobox.addItem(i);
+                }
+
+            }
+        });
+
+        startyearcombobox.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Integer.parseInt(String.valueOf(startmonthcombobox.getSelectedItem())) == 2) {
+                    int daysInMonth = 0;
+                    int year = 0;
+
+                    startdaycombobox.removeAllItems();
+                    daysInMonth = (year % 4 == 0) && (year % 100 != 0)
+                            || (year % 400 == 0) ? 29 : 28;
+                    for (int i = 1; i <= daysInMonth; i++) {
+                        startdaycombobox.addItem(i);
+
+                    }
+                }
+            }
+        });
+
+        JLabel statslut = new JLabel("Slutdato");
+        statestikNorth.add(statslut);
+        
+        JPanel statestiky = new JPanel();
+        statestikNorth.add(statestiky);
+        
+        JLabel slutday = new JLabel("Dag: ");
+        statestiky.add(slutday);
+
+        JComboBox slutdaycombobox = new JComboBox();
+        statestiky.add(slutdaycombobox);
+        slutdaycombobox.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        
+        for (int i = 1; i <= 31; i++) {
+            slutdaycombobox.addItem(i);
+        }
+        
+        JLabel slutmonth = new JLabel("Måned: ");
+        statestiky.add(slutmonth);
+        
+        JComboBox slutmonthcombobox = new JComboBox();
+        statestiky.add(slutmonthcombobox);
+        
+        for (int i = 1; i <= 12; i++) {
+            slutmonthcombobox.addItem(i);
+        }
+        
+        JLabel slutyear = new JLabel("År: ");
+        statestiky.add(slutyear);
+        
+        JComboBox slutyearcombobox = new JComboBox();
+        statestiky.add(slutyearcombobox);        
+        
+        for (int i = 2015; i <= 2021; i++) {
+            slutyearcombobox.addItem(i);
+        }
+        
+        slutmonthcombobox.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int year = Integer.parseInt(String.valueOf(slutyearcombobox.getSelectedItem()));
+                int month = Integer.parseInt(String.valueOf(slutmonthcombobox.getSelectedItem()));
+                int daysInMonth = 0;
+                switch (month) {
+                    case 1:
+                        daysInMonth = 31;
+                        break;
+                    case 2:
+                        daysInMonth = (year % 4 == 0) && (year % 100 != 0)
+                                || (year % 400 == 0) ? 29 : 28;
+                        break;
+                    case 3:
+                        daysInMonth = 31;
+                        break;
+                    case 4:
+                        daysInMonth = 30;
+                        break;
+                    case 5:
+                        daysInMonth = 31;
+                        break;
+                    case 6:
+                        daysInMonth = 30;
+                        break;
+                    case 7:
+                        daysInMonth = 31;
+                        break;
+                    case 8:
+                        daysInMonth = 31;
+                        break;
+                    case 9:
+                        daysInMonth = 30;
+                        break;
+                    case 10:
+                        daysInMonth = 31;
+                        break;
+                    case 11:
+                        daysInMonth = 30;
+                        break;
+                    case 12:
+                        daysInMonth = 31;
+                        break;
+                    default:
+                        daysInMonth = 30;
+                }
+                slutdaycombobox.removeAllItems();
+                for (int i = 1; i <= daysInMonth; i++) {
+                    slutdaycombobox.addItem(i);
+                }
+
+            }
+        });
+
+        slutyearcombobox.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Integer.parseInt(String.valueOf(slutmonthcombobox.getSelectedItem())) == 2) {
+                    int daysInMonth = 0;
+                    int year = 0;
+
+                    slutdaycombobox.removeAllItems();
+                    daysInMonth = (year % 4 == 0) && (year % 100 != 0)
+                            || (year % 400 == 0) ? 29 : 28;
+                    for (int i = 1; i <= daysInMonth; i++) {
+                        slutdaycombobox.addItem(i);
+
+                    }
+                }
             }
         });
 
