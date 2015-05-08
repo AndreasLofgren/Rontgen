76fb69e094a0cfa22e63431985dc18e76eb7b974
Stine <spirrandersen@gmail.com>
2015-05-08 10:40:16 +0200

    ekstra commit

---
+++ test/handler/AbsenceHandlerTest.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package handler;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import model.Absence;
+import model.Staff;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Andreas
+ */
+public class AbsenceHandlerTest {
+    
+    public AbsenceHandlerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getAbsence method, of class AbsenceHandler.
+     */
+    @Test
+    public void testGetAbsence() {
+        
+        AbsenceHandler ah = new AbsenceHandler();
+        ArrayList<Absence> list = ah.getAbsence();
+        int result = list.size();
+        DBHandler dbh = DBHandler.getInstance();
+        String sql = "Select count(*) From absence";
+        try{
+            Statement stmt = dbh.getStmt();
+            ResultSet rs = stmt.executeQuery(sql);
+            int expectedResult = rs.getInt(1);
+        assertEquals("Antal af elementer på listen passer ikke:",expectedResult, result);
+        } catch (Exception ex){
+            fail("getAbsence kaster exception: "+ex.getMessage());
+        }
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of insertAbsence method, of class AbsenceHandler.
+     */
+    @Test
+    public void testInsertAbsence() {
+        System.out.println("insertAbsence");
+        int id = 0;
+        Staff staff = null;
+        AbsenceHandler instance = null;
+        instance.insertAbsence(id, staff);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of updateAbsence method, of class AbsenceHandler.
+     */
+    @Test
+    public void testUpdateAbsence() {
+        System.out.println("updateAbsence");
+        String newdayStart = "";
+        String newdayEnd = "";
+        int newStaffId = 0;
+        int uniqueStaffId = 0;
+        AbsenceHandler instance = null;
+        instance.updateAbsence(newdayStart, newdayEnd, newStaffId, uniqueStaffId);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ test/handler/RoomHandlerTest.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package handler;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import model.Room;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Andreas
+ */
+public class RoomHandlerTest {
+
+    public RoomHandlerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getRoom method, of class RoomHandler.
+     */
+    @Test
+    public void testGetRoom() {
+        System.out.println("getRoom");
+        RoomHandler rh = new RoomHandler();
+        ArrayList<Room> list = rh.getRoom();
+        int result = list.size();
+        DBHandler dbh = DBHandler.getInstance();
+        String sql = "Select count(*) from room";
+        try {
+            Statement stmt = dbh.getStmt();
+            ResultSet rs = stmt.executeQuery(sql);
+            int expectedResult = rs.getInt(1);
+            assertEquals("Antal af elementer i listen passer ikke: ", expectedResult, result);
+        } catch (Exception ex) {
+            fail("getRoom kaster exception: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Test of insertRoom method, of class RoomHandler.
+     */
+//    @Test
+//    public void testInsertRoom() {
+//        System.out.println("insertRoom");
+//        int id = 0;
+//        String type = "";
+//        String status = "";
+//        int minStaffAmount = 0;
+//        int maxStaffAmount = 0;
+//        String validFrom = "";
+//        RoomHandler instance = new RoomHandler();
+//        instance.insertRoom(id, type, status, minStaffAmount, maxStaffAmount, validFrom);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of updateRoom method, of class RoomHandler.
+//     */
+//    @Test
+//    public void testUpdateRoom() {
+//        System.out.println("updateRoom");
+//        int newRoomId = 0;
+//        String newTyp = "";
+//        String newState = "";
+//        int newMinStaffAmount = 0;
+//        int newMaxStaffAmount = 0;
+//        String newValidFrom = "";
+//        int uniqueId = 0;
+//        RoomHandler instance = new RoomHandler();
+//        instance.updateRoom(newRoomId, newTyp, newState, newMinStaffAmount, newMaxStaffAmount, newValidFrom, uniqueId);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+
+}
